950ff282e19475c68e98137c99893b1e95f53186
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2021-04-27 06:56:23 +0000

    Synced to https://github.com/cs3org/cs3apis/tree/62377dd2ce0cf8f286ce1a6b13dbecc7c5df262f

--- cs3/app/provider/v1beta1/provider_api_grpc_web_pb.js
+++ cs3/app/provider/v1beta1/provider_api_grpc_web_pb.js
@@ -169,5 +169,79 @@ proto.cs3.app.provider.v1beta1.ProviderAPIPromiseClient.prototype.openFileInAppP
 };
 
 
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.cs3.app.provider.v1beta1.OpenInAppRequest,
+ *   !proto.cs3.app.provider.v1beta1.OpenInAppResponse>}
+ */
+const methodDescriptor_ProviderAPI_OpenInApp = new grpc.web.MethodDescriptor(
+  '/cs3.app.provider.v1beta1.ProviderAPI/OpenInApp',
+  grpc.web.MethodType.UNARY,
+  proto.cs3.app.provider.v1beta1.OpenInAppRequest,
+  proto.cs3.app.provider.v1beta1.OpenInAppResponse,
+  /** @param {!proto.cs3.app.provider.v1beta1.OpenInAppRequest} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.cs3.app.provider.v1beta1.OpenInAppResponse.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.cs3.app.provider.v1beta1.OpenInAppRequest,
+ *   !proto.cs3.app.provider.v1beta1.OpenInAppResponse>}
+ */
+const methodInfo_ProviderAPI_OpenInApp = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.cs3.app.provider.v1beta1.OpenInAppResponse,
+  /** @param {!proto.cs3.app.provider.v1beta1.OpenInAppRequest} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.cs3.app.provider.v1beta1.OpenInAppResponse.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.cs3.app.provider.v1beta1.OpenInAppRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.cs3.app.provider.v1beta1.OpenInAppResponse)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.cs3.app.provider.v1beta1.OpenInAppResponse>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.cs3.app.provider.v1beta1.ProviderAPIClient.prototype.openInApp =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/cs3.app.provider.v1beta1.ProviderAPI/OpenInApp',
+      request,
+      metadata || {},
+      methodDescriptor_ProviderAPI_OpenInApp,
+      callback);
+};
+
+
+/**
+ * @param {!proto.cs3.app.provider.v1beta1.OpenInAppRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.cs3.app.provider.v1beta1.OpenInAppResponse>}
+ *     A native promise that resolves to the response
+ */
+proto.cs3.app.provider.v1beta1.ProviderAPIPromiseClient.prototype.openInApp =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/cs3.app.provider.v1beta1.ProviderAPI/OpenInApp',
+      request,
+      metadata || {},
+      methodDescriptor_ProviderAPI_OpenInApp);
+};
+
+
 module.exports = proto.cs3.app.provider.v1beta1;
 
